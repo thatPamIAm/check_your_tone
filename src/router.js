6a5a6e6cc585872db6bf910d8a93d76ea37658be
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-04-11 13:06:28 -0600

    Comment out file for potential deletion

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('./controller');
-
-router.get('/places', controller.getPlaces)
-
-module.exports = router;
+// var express = require('express');
+// var router = express.Router();
+// var controller = require('./controller');
+//
+// router.get('/places', controller.getPlaces)
+//
+// module.exports = router;
