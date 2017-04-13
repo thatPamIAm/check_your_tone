8bf5a8df1460be3b9377a2d7034bf3c88c896b05
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-04-12 20:25:52 -0600

    test heroku

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000);
 const app = express();
 const request = require('request');
-var watson = require('watson-developer-cloud');
+// var watson = require('watson-developer-cloud');
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); //should this be false???
@@ -27,63 +29,30 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use(express.static('app'));
 
-//`./${process.env.INDEX_SRC}`
 app.get('/', function (req, res) { res.sendFile(path.join(__dirname, './index.html')) });
 app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, './index.html')) });
-// app.get('/', function(req, res) {res.send('Running!!'); });
 
 app.listen(port);
 
-
-//req.params??? and pass in username/password/text
-//ibm watson requires header and body
 app.post('/post', function(req, res){
   console.log('post is working')
   var text = req.body.text;
   console.log(text)
 
-  var tone_analyzer = watson.tone_analyzer({
+  var tone_analyzer = new ToneAnalyzerV3({
     username: process.env._USERNAME,
     password: process.env._PASSWORD,
     version: 'v3',
     version_date: '2016-05-19 '
   });
 
-  //options for get
-  //host
-  //port
-  //path
-  //method
-  // request(payload, function(error, response, body) {
-    tone_analyzer.tone({ text: text },
-      function(err, tone) {
-        if (err)
-        console.log(err);
-        else
-        res.send(JSON.stringify(tone, null, 2));
-      });
-    // })
+  tone_analyzer.tone({ text: text },
+    function(err, tone) {
+      if (err)
+      console.log(err);
+      else
+      res.send(JSON.stringify(tone, null, 2));
   });
-// res.send(body)
+});
 
-// app.post('/post', function(req, res){
-//   var text = req.body.text;
-//
-//   var payload = {
-//     text: text
-//   }
-//
-//   request(payload, function(error, response, body) {
-//     if(!error && response.code === 200){
-//       var test = 'testing this shhhiiiiiitt'
-//
-//       var body = {
-//         response_type: "in_channel",
-//         text: test
-//       };
-//       res.send(body);
-//     }
-//   })
-// });
-
-console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
