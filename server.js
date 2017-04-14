8202cd67c5aaac3182847cffbe76020c87385329
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-04-13 18:05:30 -0600

    test slack

--- server.js
+++ server.js
@@ -39,8 +39,11 @@ app.post('/post', function(req, res){
   var text = req.body.text;
 //instantiation of a new object with credentials
   var tone_analyzer = new ToneAnalyzerV3({
-    username: process.env._USERNAME,
-    password: process.env._PASSWORD,
+    username: "fb839465-02ce-4473-9d1e-e66acdc3b871",
+    password: 'g7jdAQ0qBqDG',
+
+    // username: process.env._USERNAME,
+    // password: process.env._PASSWORD,
     version: 'v3',
     version_date: '2016-05-19 '
   });
@@ -53,7 +56,32 @@ app.post('/post', function(req, res){
       else
       console.log('hit else')
       var postToSlack = tone.document_tone.tone_categories[0].tones.map(key => {
-        return key.tone_name + " " + key.score;
+        return {
+          "color": "#36a64f",
+          "text": `${key.tone_name}./n${key.score}`,
+        }
+
+        // {
+        //             "fallback": "Required plain-text summary of the attachment.",
+        //             "color": "#36a64f",
+        //             "pretext": "Optional text that appears above the attachment block",
+        //             "author_name": "Bobby Tables",
+        //             "author_link": "http://flickr.com/bobby/",
+        //             "author_icon": "http://flickr.com/icons/bobby.jpg",
+        //             "title": "Slack API Documentation",
+        //             "title_link": "https://api.slack.com/",
+        //             "text": "Optional text that appears within the attachment",
+        //             "fields": [
+        //                 {
+        //                     "title": "Priority",
+        //                     "value": "High",
+        //                     "short": false
+        //                 }
+        //             ],
+        //             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+        //             "ts": 123456789
+        //         }
+        // return key.tone_name + " " + key.score;
         console.log('it should totally be working')
       });
       console.log(postToSlack)
